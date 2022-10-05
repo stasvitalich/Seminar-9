42eb94fb5280da6658577a9d572470b6b5f3e58b
Stanislav <stasvitalich1994@gmail.com>
2022-10-05 16:25:27 +0600

    Sum of elements between two numbers

--- TASK 66/Program.cs
+++ TASK 66/Program.cs
@@ -1,1 +1,44 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Задача 66.
+// Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// Пример: 
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+try
+{
+    int m = GetNumbers("Введите m: ");
+    int n = GetNumbers("Введите n (должно быть больше m): ");
+    int temp = m;
+
+    if (m > n)
+    {
+        m = n;
+        n = temp;
+    }
+
+    GetSumm(m, n, temp = 0);
+
+    void GetSumm(int m, int n, int summ)
+    {
+        summ = summ + n;
+        if (n <= m)
+        {
+            Console.Write($"Сумма элементов = {summ} ");
+            return;
+        }
+        GetSumm(m, n - 1, summ);
+    }
+
+    int GetNumbers(string input)
+    {
+        Console.Write(input);
+        int output = int.Parse(Console.ReadLine());
+        return output;
+    }
+}
+
+catch (Exception ex)
+{
+    Console.WriteLine($"Ошибка: вы не ввели натурльное число. Попробуйте снова. Error: {ex.Message}");
+}
